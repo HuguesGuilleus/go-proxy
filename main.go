4539b37f19221a93dd1488ff837096297e09fab9
hugues <ghugues@netc.fr>
2021-04-26 15:04:00 +0200

    use a separated handler

--- main.go
+++ main.go
@@ -11,45 +11,42 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"strings"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Hijack support
+var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodConnect {
+		log.Println("Request", r.Method, r.URL)
+		rep, err := http.DefaultTransport.RoundTrip(r)
+		if err != nil {
+			log.Println("Error", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		header := w.Header()
+		for k, vv := range rep.Header {
+			for _, v := range vv {
+				header.Add(k, v)
+			}
+		}
+		w.WriteHeader(rep.StatusCode)
+		io.Copy(w, rep.Body)
+	} else {
 		h, ok := w.(http.Hijacker)
 		if !ok {
 			log.Println("Error: No hijack")
 			http.Error(w, "Need Hijacker connexion", http.StatusInternalServerError)
 			return
-		} else if r.Method != http.MethodConnect {
-			http.Error(w, "Need the HTTP method CONNECT", http.StatusMethodNotAllowed)
-			return
 		}
 
 		// Connexion to the target
-		a := r.URL.Host
-		if !strings.ContainsRune(a, ':') {
-			switch r.URL.Scheme {
-			case "http":
-				a += ":80"
-			case "https":
-				a += ":443"
-			default:
-				log.Println("Error: wrong url sheme")
-				http.Error(w,
-					fmt.Sprintf("Unknown the scheme %q (use http, https or print the port)", r.URL.Scheme),
-					http.StatusBadRequest)
-			}
-		}
-		target, err := net.Dial("tcp", a)
+		target, err := net.Dial("tcp", r.URL.Host)
 		if err != nil {
 			log.Println("Error:", err)
-			http.Error(w, fmt.Sprintf("Dial to %q fail: %v\n", a, err), http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("Dial to %q fail: %v\n", r.URL.Host, err), http.StatusInternalServerError)
 			return
 		}
 		defer target.Close()
-		log.Println("Connexion", a)
+		log.Println("Connexion", r.URL.Host)
 
 		// Take the connexion
 		client, _, err := h.Hijack()
@@ -58,7 +55,7 @@ func main() {
 			return
 		}
 		defer client.Close()
-		client.Write([]byte("200 Connexion Establish\r\n"))
+		client.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
 		// Bidiretionnal copy
 		done := make(chan struct{}, 1)
@@ -71,11 +68,13 @@ func main() {
 			done <- struct{}{}
 		}()
 		<-done
-	})
+	}
+}
 
+func main() {
 	l := flag.String("l", ":5000", "Listen address")
 	flag.Parse()
 
 	log.Println("Listen", *l)
-	log.Fatal(http.ListenAndServe(*l, nil))
+	log.Fatal(http.ListenAndServe(*l, handler))
 }
